c3b03a48dc8905b365e608850836a059745d15cf
Bharath Ram <8543486+rabhar@users.noreply.github.com>
2020-11-24 03:58:14 +0530

    updated delete file

--- DeleteFileUpdated.java
+++ DeleteFileUpdated.java
@@ -3,40 +3,37 @@ import java.io.FileReader;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import java.nio.file.*;
-import java.io.FileWriter;
+
 
 class DeleteFile{
 
     public static void main(String[] args) {
         File directory = new File("allure-results");
 
-        // Get all files from a directory.
         File[] fList = directory.listFiles();
+        JSONParser parser = new JSONParser();
+
         for(File f: fList){
-            //System.out.println(f.getName());
-            JSONParser parser = new JSONParser();
+
             try { 
-                Object obj = parser.parse(new FileReader(f.getAbsolutePath())); 
+                FileReader reader =  new FileReader(f.getAbsolutePath());
+                Object obj = parser.parse(reader); 
+                reader.close();
 
                 JSONObject jsonObject =  (JSONObject) obj;
 
                 String name = (String) jsonObject.get("status");
-                //System.out.println(name);  
-                JSONArray cars = (JSONArray) jsonObject.get("steps");
-                int s = cars.size();
-                //System.out.println(s);
-                if(name.equals("failed") && s == 0){
-                    System.out.println(f.getAbsolutePath());
-                    FileWriter myWriter = new FileWriter(f.getAbsolutePath());
-                    myWriter.write("{}");
-                    myWriter.close();                
-                }
+                JSONArray steps = (JSONArray) jsonObject.get("steps");
+                int stepsSize = steps.size();
                 
+                if(name.equals("failed") && stepsSize == 0){
+                    System.out.println(f.getAbsolutePath());
+                    File file = new File(f.getAbsolutePath());
+                    file.delete();
+                }
             } catch (Exception e) {
                 //System.out.println("exception : " + f.getName());
-            }
-                        
+            }        
         }
     }
 }
